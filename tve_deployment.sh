95728b426f78daecf1a98248c85a64d1dc6d8761
Aliaksei Yakhnenka <aliaksei_yakhnenka@epam.com>
2014-01-13 15:01:04 +0300

    many updates

--- tve_deployment.sh
+++ tve_deployment.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 # Requirements: cloned repositories into acquia, nbcutve and publisher7 respectively
 
-# initialize
+################### initialize ###################
 clear
-UPDATE=0
-echo "Starting deployment process."
+echo "Initializing deployment process."
 echo ""
+UPDATE=0
 
 # update tags from acquia repository
 cd acquia
-echo "Fetching latest tags from Acquia repository..."
+echo "Updating local Acquia repository..."
 git fetch --tags
-echo "Done"
+echo "Done."
 echo ""
 
 # get the latest acquia build tag
 LATEST_BUILD_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo "Current build tag: $LATEST_BUILD_TAG"
+LATEST_BUILD_TAG_PARSED=(${LATEST_BUILD_TAG//-/ })
+echo "Latest build tag: $LATEST_BUILD_TAG"
+echo ""
+echo "Build info: "
+echo "${LATEST_BUILD_TAG//-/$'\n'}"
 echo ""
 cd ..
-arrIN=(${LATEST_BUILD_TAG//-/ })
+<<COMMENT1
+LATEST_BUILD_TAG_PARSED=(${LATEST_BUILD_TAG//-/ })
 
 # parse build version
-BUILD_VER_LATEST=${arrIN[0]}
-BUILD_VER_LATEST=(${BUILD_VER_LATEST//build/ })
-BUILD_VER_LATEST=${BUILD_VER_LATEST[0]}
+LAST_BUILD_VERSION=${LATEST_BUILD_TAG_PARSED[0]}
+LAST_BUILD_VERSION=(${LAST_BUILD_VERSION//build/ })
+LAST_BUILD_VERSION=${LAST_BUILD_VERSION[0]}
 
 # parse publisher7 version
-P7_VER_LATEST=${arrIN[1]}
-P7_VER_LATEST=(${P7_VER_LATEST//p/ })
-P7_VER_LATEST=${P7_VER_LATEST[0]}
+P7_VERSION_LATEST=${LATEST_BUILD_TAG_PARSED[1]}
+P7_VERSION_LATEST=(${P7_VERSION_LATEST//p/ })
+P7_VERSION_LATEST=${P7_VERSION_LATEST[0]}
 
 # parse tve version
-TVE_VER_LATEST=${arrIN[2]}
+TVE_VER_LATEST=${LATEST_BUILD_TAG_PARSED[2]}
 TVE_VER_LATEST=(${TVE_VER_LATEST//nbcutve/ })
 TVE_VER_LATEST=${TVE_VER_LATEST[0]}
 
 # parse git branch
-BRANCH_LATEST=${arrIN[3]}
+BRANCH_LATEST=${LATEST_BUILD_TAG_PARSED[3]}
 
 # check if all version tags extracted successfully
-if [ $BUILD_VER_LATEST ] || [ $P7_VER_LATEST ] || [ $TVE_VER_LATEST ] || [ $BRANCH_LATEST ] ; then
-  echo "Build:      $BUILD_VER_LATEST"
-  echo "Publisher7: $P7_VER_LATEST"
+if [ $LAST_BUILD_VERSION ] || [ $P7_VERSION_LATEST ] || [ $TVE_VER_LATEST ] || [ $BRANCH_LATEST ] ; then
+  echo "Build:      $LAST_BUILD_VERSION"
+  echo "Publisher7: $P7_VERSION_LATEST"
   echo "NBCU TVE:   $TVE_VER_LATEST"
   echo "Branch:     $BRANCH_LATEST"
   echo ""
@@ -50,44 +55,63 @@ else
   echo "Error: Could not parse latest build tag."
   exit
 fi
+COMMENT1
 
-# setup build version
-read -n 1 -p "Press 'y' to start new build deployment: " AMSURE
-[ "$AMSURE" = "y" ] || exit
+
+read -n 1 -p "Start new build deployment? (y/n): " CONFIRM
+[ "$CONFIRM" = "y" ] || exit
 echo ""
-NEXT_BUILD_VER=$(($BUILD_VER_LATEST+1))
 
-read -n 1 -p "Next build version will be: $NEXT_BUILD_VER. Press 'y' to change: " CONFIRM
+# get last build version
+LAST_BUILD_VERSION=${LATEST_BUILD_TAG_PARSED[0]}
+LAST_BUILD_VERSION=(${LAST_BUILD_VERSION//build/ })
+LAST_BUILD_VERSION=${LAST_BUILD_VERSION[0]}
+
+# setup new build version
+NEXT_BUILD_VERSION=$(($LAST_BUILD_VERSION+1))
+
+echo "Next build version will be: $NEXT_BUILD_VERSION"
+read -n 1 -p "Change build version? (y/n): " CONFIRM
 echo ""
 if [ "$CONFIRM" = "y" ] ; then
   echo "Enter new build version:"
-  read BUILD_VER
-else
-  BUILD_VER=$NEXT_BUILD_VER
+  read NEXT_BUILD_VERSION
 fi
 
-echo "Build version is set to $BUILD_VER"
+BUILD_VERSION=$NEXT_BUILD_VERSION
+
+echo "Build version is set to $BUILD_VERSION"
 echo ""
 
-# setup publisher
-read -n 1 -p "Publisher7 (forked) version is: $P7_VER_LATEST. Press 'y' to change: " AMSURE 
+
+
+################### setup publisher7 ###################
+
+# parse publisher7 version
+P7_VERSION_LATEST=${LATEST_BUILD_TAG_PARSED[1]}
+P7_VERSION_LATEST=(${P7_VERSION_LATEST//p/ })
+P7_VERSION_LATEST=${P7_VERSION_LATEST[0]}
+
+echo "Publisher7 (forked) version is: $P7_VERSION_LATEST"
+read -n 1 -p "Change Publisher7 version? (y/n): " CONFIRM
 echo ""
 
-if [ "$AMSURE" = "y" ] ; then
+if [ "$CONFIRM" = "y" ] ; then
   cd publisher7
   echo "Pulling code from forked Publisher7 git repository..."
   git fetch origin
   git checkout tve || (echo "Could not checkout master" && exit 1)
   git pull --ff-only origin tve || (echo "Could not pull. Merge was likely not a fast forward" && exit 1)
-  read -n 1 -p "List all forked Publisher7 tags? (y/[a]): " AMSURE 
-  echo "" 1>&2
-  if [ "$AMSURE" = "y" ] ; then
+  read -n 1 -p "List all forked Publisher7 tags? (y/n): " CONFIRM 
+  echo ""
+  if [ "$CONFIRM" = "y" ] ; then
     git tag
   fi
+
+  read -n 1 -p "Proceed with Publisher7 update? (y/n): " CONFIRM 
+  echo ""
   
-  read -n 1 -p "Proceed with Publisher7 update? (y/[a]): " AMSURE 
-  echo "" 1>&2
-  if [ "$AMSURE" = "y" ] ; then
+  if [ "$CONFIRM" = "y" ] ; then
     read -p "Publisher7 tag to checkout: " P7_VER 
     echo "Checkout..."
     git checkout $P7_VER
@@ -100,58 +124,94 @@ if [ "$AMSURE" = "y" ] ; then
     P7_VER=${P7_VER[0]}
     UPDATE=1
   else
-    P7_VER=$P7_VER_LATEST
+    P7_VER=$P7_VERSION_LATEST
     echo ""
   fi
   
   cd ..
 else
-  P7_VER=$P7_VER_LATEST
+  P7_VER=$P7_VERSION_LATEST
   echo ""
 fi
 
-read -n 1 -p "Press 'y' to deploy TVE: " AMSURE 
+
+
+################### setup tve ###################
+
+read -n 1 -p "Deploy TVE? (y/n): " CONFIRM 
 echo ""
 
-if [ "$AMSURE" = "y" ] ; then
+if [ "$CONFIRM" = "y" ] ; then
   cd nbcutve
-  
-  # setup tve branch
-  LOCAL_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-  echo "Current branch is: $LOCAL_BRANCH"
+  # setup branch
+  #LOCAL_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+  LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  echo "Current branch: $LOCAL_BRANCH"
   BRANCH_LATEST=$LOCAL_BRANCH
-  read -n 1 -p "Switch to other branch? (y/n): " AMSURE 
+  read -n 1 -p "Switch to other branch? (y/n): " CONFIRM 
   echo ""
-  if [ "$AMSURE" = "y" ] ; then
+  if [ "$CONFIRM" = "y" ] ; then
+    echo "Updating local TVE repository..."
 	git fetch origin
+	echo "Done."
+	echo ""
 	echo "Available branches:"
 	git branch --list -a
 	echo ""
-	read -p "Type in branch name: " LOCAL_BRANCH
+	echo "Enter branch name: "
+	read LOCAL_BRANCH
+	echo ""
 	echo "Switching to branch: $LOCAL_BRANCH"
 	git checkout "$LOCAL_BRANCH"
 	BRANCH_LATEST=$LOCAL_BRANCH
   fi
+  
+  echo "Updating branch $BRANCH_LATEST..."
+  git pull origin "$BRANCH_LATEST"
+  echo "Done."
 
-  read -p "Set TVE version (current $TVE_VER_LATEST): " TVE_VER
-  echo "TVE version is set to $TVE_VER"
-  echo "Pulling code from TVE git repository..."
-  git pull origin "$LOCAL_BRANCH"
-  read -n 1 -p "Proceed with files sync for TVE? (y/[a]): " AMSURE 
+  # detect tag prefix  
+  TAG_PREFIX="dev"
+  if [[ "$BRANCH_LATEST" == "master" ]] ; then
+    TAG_PREFIX="r"
+  fi
+  
+  if [[ "$BRANCH_LATEST" == "dev" ]] ; then
+    TAG_PREFIX="dev"
+  fi
+  
+  if [[ "$BRANCH_LATEST" == "release_candidate"* ]] ; then
+    TAG_PREFIX="rc"
+  fi
+  
+  echo "TVE tag prefix: $TAG_PREFIX"
+  echo "Previous tags with this prefix: "
+  
+  BRANCH_TAGS=$(git tag -l "$TAG_PREFIX"*)
+  echo "$BRANCH_TAGS"
   echo ""
   
-  if [ "$AMSURE" = "y" ] ; then
-	read -n 1 -p "Push tag v$TVE_VER? (y/[a]): " AMSURE 
+  read -p "Enter new TVE tag number (without prefix): " TVE_VER
+  echo "TVE version is set to $TVE_VER"
+ 
+  TVE_TAG="$TAG_PREFIX$TVE_VER"
+ 
+  read -n 1 -p "Proceed with files sync for TVE? (y/n): " CONFIRM 
+  echo ""
+  
+  if [ "$CONFIRM" = "y" ] ; then
+	read -n 1 -p "Push tag $TVE_TAG? (y/n): " CONFIRM 
 	echo ""
-	if [ "$AMSURE" = "y" ] ; then
-	  # Tag current code and push it back to TVE repo
-	  git tag "v$TVE_VER"
-      git push -u origin "v$TVE_VER"
+	if [ "$CONFIRM" = "y" ] ; then
+	  # tag current code and push it back to TVE repo
+	  git tag "$TVE_TAG"
+      git push -u origin "$TVE_TAG"
 	fi
 	
-	# Export changelog to a file
-	git log "v$TVE_VER_LATEST".."v$TVE_VER" --merges --format="Date: %ci%nAuthor: %an (%ae)%nCommit: %s%nDescription: %b%n-------------------------%n" > ../changelogs/changelog."$BRANCH_LATEST"."v$TVE_VER_LATEST"__"v$TVE_VER".txt
+	# export changelog to a file
+	git log "v$TVE_VER_LATEST".."v$TVE_VER" --merges --format="Date: %ci%nAuthor: %an (%ae)%nCommit: %s%nDescription: %b%n-------------------------------%n" > ../changelogs/changelog."$BRANCH_LATEST"."v$TVE_VER_LATEST"__"v$TVE_VER".txt
 	
+	# rsync files
     echo "Synchronizing TVE code to acquia/ directory..."
     rsync -aq --exclude ".git/" --exclude ".gitignore" --exclude "default" --exclude "mvpdadmin" --exclude "all/drush" --exclude "all/libraries/ckeditor" --exclude "all/libraries/facebook-php-sdk" --exclude "all/libraries/plupload" --exclude "all/libraries/postscribe" --exclude "all/libraries/smartirc" --exclude "all/libraries/tinymce" --exclude "all/libraries/writecapture" --exclude "all/libraries/zend_crypt" --delete --force . ../acquia/docroot/sites
     cd ..
@@ -162,15 +222,18 @@ else
   echo ""
 fi
 
+
+################### deploy the build ###################
+
 if [ "$UPDATE" = 1 ] ; then
-  # Deploy code to Acqia hosting
   cd acquia
-  BUILD_TAG="build${BUILD_VER}-p${P7_VER}-nbcutve${TVE_VER}-${BRANCH_LATEST}"
+  #BUILD_TAG="build${BUILD_VERSION}-p${P7_VER}-nbcutve${TVE_VER}-${BRANCH_LATEST}"
+  BUILD_TAG="build_${BUILD_VERSION}-tve_${TVE_VER}-pub_${P7_VER}"
   git add -A
   git status
-  read -n 1 -p "Commit these changes under $BUILD_TAG tag? (y/[a]): " AMSURE 
+  read -n 1 -p "Press 'y' to commit these changes under $BUILD_TAG: " CONFIRM 
   echo ""
-  if [ "$AMSURE" = "y" ] ; then
+  if [ "$CONFIRM" = "y" ] ; then
     git commit -m "$BUILD_TAG"
     git tag $BUILD_TAG
     git push -u origin $BUILD_TAG
